bc70a0f32152dd606c143d324850f459f4d129bc
lucasheld <lucasheld@hotmail.de>
2021-05-04 16:05:53 +0200

    add app bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,111 @@
-import React from "react";
+import React, {Component} from 'react';
+import {
+    AppBar,
+    CssBaseline,
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+    Typography,
+    withStyles
+} from "@material-ui/core";
+import {DeviceHub, Memory} from "@material-ui/icons";
 import DevicesComponent from "./components/DevicesComponent";
 import FirmwaresComponent from "./components/FirmwaresComponent";
 
-function App() {
-  return (
-    <div className="App">
-        <p>
-            <h1>Devices</h1>
-            <DevicesComponent/>
-        </p>
-        <p>
-            <h1>Firmwares</h1>
-            <FirmwaresComponent/>
-        </p>
-    </div>
-  );
+const drawerWidth = 200;
+
+const styles = theme => ({
+    root: {
+        display: 'flex'
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    drawerContainer: {
+        overflow: 'auto'
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3)
+    },
+    title: {
+        flexGrow: 1
+    },
+});
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayComponent: <DevicesComponent/>,
+        };
+    }
+
+    displayDevices = () => {
+        this.setState({
+            displayComponent: <DevicesComponent/>
+        });
+    };
+
+    displayFirmwares = () => {
+        this.setState({
+            displayComponent: <FirmwaresComponent/>
+        });
+    };
+
+    render() {
+        const {classes} = this.props;
+
+        return (
+            <div className={classes.root}>
+                <CssBaseline/>
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <Typography variant="h6" noWrap className={classes.title}>espcd-frontend</Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    className={classes.drawer}
+                    variant="permanent"
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <Toolbar/>
+                    <div className={classes.drawerContainer}>
+                        <List>
+                            <ListItem button key="devices" onClick={this.displayDevices}>
+                                <ListItemIcon>
+                                    <DeviceHub/>
+                                </ListItemIcon>
+                                <ListItemText primary="Devices"/>
+                            </ListItem>
+                            <ListItem button key="firmwares" onClick={this.displayFirmwares}>
+                                <ListItemIcon>
+                                    <Memory/>
+                                </ListItemIcon>
+                                <ListItemText primary="Firmwares"/>
+                            </ListItem>
+                        </List>
+                    </div>
+                </Drawer>
+                <main className={classes.content}>
+                    <Toolbar/>
+                    {this.state.displayComponent}
+                </main>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withStyles(styles)(App);

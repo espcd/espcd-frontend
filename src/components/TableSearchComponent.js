d2e6f51e85af7990b74d8c0889a72ae3256f678f
lucasheld <lucasheld@hotmail.de>
2021-08-07 16:44:00 +0200

    small style changes

--- src/components/TableSearchComponent.js
+++ src/components/TableSearchComponent.js
@@ -1,16 +1,28 @@
 import React, {Component} from "react";
-import {Grid, IconButton, InputAdornment, TextField} from "@material-ui/core";
+import {Grid, IconButton, InputAdornment, TextField, withStyles} from "@material-ui/core";
 import {Clear} from "@material-ui/icons";
+
+const styles = () => ({
+    grid: {
+        alignItems: "center"
+    },
+    input: {
+        minWidth: "110px"
+    },
+});
 
 class TableSearchComponent extends Component {
     render() {
+        const {classes} = this.props;
+
         return (
-            <Grid container style={{alignItems: "center"}} justify="flex-end">
+            <Grid container className={classes.grid} justify="flex-end">
                 <TextField
                     label="Search..."
                     value={this.props.query}
                     onChange={event => this.props.setQuery(event.target.value)}
                     InputProps={{
+                        className: classes.input,
                         endAdornment: (
                             <InputAdornment position="end">
                                 <IconButton onClick={() => this.props.setQuery("")}>
@@ -25,4 +37,4 @@ class TableSearchComponent extends Component {
     }
 }
 
-export default TableSearchComponent;
+export default withStyles(styles)(TableSearchComponent);
